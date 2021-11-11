7497a73a71688ab2636818fd2f93a80009cc285b
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-11-11 19:50:13 +0530

    Update Manoarya.js

--- Manoarya.js
+++ Manoarya.js
@@ -1,24 +1,17 @@
 [
-   {
-    "login": "mojombo",
-    "id": 1,
-    "node_id": "MDQ6VXNlcjE=",
-    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/mojombo",
-    "html_url": "https://github.com/mojombo",
-    "followers_url": "https://api.github.com/users/mojombo/followers",
-    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
-    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
-    "organizations_url": "https://api.github.com/users/mojombo/orgs",
-    "repos_url": "https://api.github.com/users/mojombo/repos",
-    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/mojombo/received_events",
-    "type": "User",
-    "site_admin": false
-  },
+    {
+      'category': "Notes",
+      'Title': "How To Make a website using Html and Css",
+      'Paragraph':
+        " et cillum eiusmod. Laborum ipsum laborum eiusmod amet ullamco dolor pariatur do.",
+      'ImageUrl':
+        "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      'PostUrl': "https://classnotes.tech",
+      'alt': "coding",
+      'trending': "true"
+    }
+   
   
   ];
   
+
