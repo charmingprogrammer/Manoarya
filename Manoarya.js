9e3f3b80d3eb190fbff62866f112c011345edb23
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-11-11 22:26:24 +0530

    Update Manoarya.js

--- Manoarya.js
+++ Manoarya.js
@@ -7,6 +7,122 @@
       "PostUrl": "https://classnotes.tech",
       "alt": "coding",
       "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },  {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    },
+      {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
     }
   ]
   
