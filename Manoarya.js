336d925dbd8e3c621485e3b9d86aab54b0613822
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-11-11 20:17:08 +0530

    Update Manoarya.js

--- Manoarya.js
+++ Manoarya.js
@@ -1,23 +1,12 @@
 [
-  {
-    "name":"Manohar Anand",
-    "login": "bmizerany",
-    "id": 46,
-    "node_id": "MDQ6VXNlcjQ2",
-    "avatar_url": "https://avatars.githubusercontent.com/u/46?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/bmizerany",
-    "html_url": "https://github.com/bmizerany",
-    "followers_url": "https://api.github.com/users/bmizerany/followers",
-    "following_url": "https://api.github.com/users/bmizerany/following{/other_user}",
-    "gists_url": "https://api.github.com/users/bmizerany/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/bmizerany/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/bmizerany/subscriptions",
-    "organizations_url": "https://api.github.com/users/bmizerany/orgs",
-    "repos_url": "https://api.github.com/users/bmizerany/repos",
-    "events_url": "https://api.github.com/users/bmizerany/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/bmizerany/received_events",
-    "type": "User",
-    "site_admin": false
-  }
-]
+    {
+      "category":"Notes",
+      "Title": "How To Make a website using Html and Css",
+      "Paragraph": "Elit magna laborum commodo officia. Adipisicing officia cillum enim aute proident ipsum cillum anim in elit ipsum. Lorem amet aliqua ut exercitation consectetur.",
+      "ImageUrl": "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      "PostUrl": "https://classnotes.tech",
+      "alt": "coding",
+      "trending": "true"
+    }
+  ]
+  
