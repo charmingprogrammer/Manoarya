235569293b2565bff7d103ba0d1fcc5a0c2dffb1
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-11-11 19:31:36 +0530

    Create Manoarya.js

---
+++ Manoarya.js
@@ -0,0 +1,16 @@
+[
+    {
+      category: "Notes",
+      Title: "How To Make a website using Html and Css",
+      Paragraph:
+        " et cillum eiusmod. Laborum ipsum laborum eiusmod amet ullamco dolor pariatur do.",
+      ImageUrl:
+        "https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_960_720.jpg",
+      PostUrl: "https://classnotes.tech",
+      alt: "coding",
+      trending: "true",
+    },
+   
+  
+  ];
+  
