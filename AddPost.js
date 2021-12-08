d6eba1ed41d5b672386278019c4d76e4d8c80667
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-12-08 08:36:23 +0530

    update AddPost.js

--- AddPost.js
+++ AddPost.js
@@ -3,10 +3,10 @@ const AllPost = [
       category: "HTM",
       Title: "How to make Sidebar with multiple Pages using HTML, CSS and Javascript",
       Paragraph:
-        "",
+        "Learn How to make a awesome sidebar with multiple Pages for free source code.",
       ImageUrl:
         "https://charmingprogrammer.github.io/All-Manoarya-Images/Navigation.png",
-      PostUrl: "https://classnotes.tech",
+      PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/manoaryaPost.html",
       alt: "Navigation by Manoarya",
       trending: "true",
     }
