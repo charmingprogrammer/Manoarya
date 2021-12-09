fd98f2658aefeac487635580515b5362c37200fc
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-12-09 07:50:07 +0530

    update AddPost.js

--- AddPost.js
+++ AddPost.js
@@ -9,7 +9,18 @@ const AllPost = [
       PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/manoaryaPost.html",
       alt: "Navigation by Manoarya",
       trending: "true",
-    }
+    },
+        {
+      category: "HTM",
+      Title: "How to make Modal Box using HTML,CSS and Javascript?",
+      Paragraph:
+        "Learn How to make a awesome Modal Box with free source code.",
+      ImageUrl:
+        "https://cdn.pixabay.com/photo/2021/12/08/17/04/17-04-02-625_1280.jpg",
+      PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_modal_box_using_html_css_and_js.html",
+      alt: "Modal Box by Manoarya",
+      trending: "true",
+    },
    
   
   ];
