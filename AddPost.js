8fa822cfdc1613ffa3915a345fcc4d062fb305ce
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-12-09 08:13:55 +0530

    update AddPost.js

--- AddPost.js
+++ AddPost.js
@@ -16,7 +16,7 @@ const AllPost = [
       Paragraph:
         "Learn How to make a awesome Modal Box with free source code.",
       ImageUrl:
-        "https://cdn.pixabay.com/photo/2021/12/08/17/04/17-04-02-625_1280.jpg",
+        "https://cdn.pixabay.com/photo/2021/12/08/17/04/modalbox-6856321_1280.jpg",
       PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_modal_box_using_html_css_and_js.html",
       alt: "Modal Box by Manoarya",
       trending: "true",
