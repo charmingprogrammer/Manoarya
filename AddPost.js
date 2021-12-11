e669f86508b5cf31dcb802cbc6cbf3d2e7348772
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-12-11 06:40:49 +0530

    update AddPost.js

--- AddPost.js
+++ AddPost.js
@@ -1,39 +1,36 @@
-const AllPost = [
-    {
-      category: "HTM",
-      Title: "How to make Sidebar with multiple Pages using HTML, CSS and Javascript?",
-      Paragraph:
-        "Learn How to make a awesome sidebar with multiple Pages for free source code.",
-      ImageUrl:
-        "https://charmingprogrammer.github.io/All-Manoarya-Images/Navigation.png",
-      PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-make-Sidebar-with-multiple-Pages-using-HTML-CSS-and-Javascript.html",
-      alt: "Navigation by Manoarya",
-      trending: "true",
-    },
-        {
-      category: "HTM",
-      Title: "How to make Modal Box using HTML,CSS and Javascript?",
-      Paragraph:
-        "Learn How to make a awesome Modal Box with free source code.",
-      ImageUrl:
-        "https://cdn.pixabay.com/photo/2021/12/08/17/04/modalbox-6856321_1280.jpg",
-      PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_modal_box_using_html_css_and_js.html",
-      alt: "Modal Box by Manoarya",
-      trending: "true",
-    },
-            {
-      category: "HTM",
-      Title: "How to make Information opener using HTML, CSS and Js? for free.",
-      Paragraph:
-        "Learn How to make a awesome Information opener with free source code.",
-      ImageUrl:
-        "https://i.ibb.co/Hn2wVnv/Pics-Art-12-09-08-14-16.png",
-      PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_info_opener_using_html_css_and_js.html",
-      alt: "Information opener by Manoarya",
-      trending: "true",
-    },
-   
-  
-  ];
-  
-  
+const AllPost = [{
+	category: "HTM",
+	Title: "How to make Sidebar with multiple Pages using HTML, CSS and Javascript?",
+	Paragraph:
+	"Learn How to make a awesome sidebar with multiple Pages for free source code.",
+	ImageUrl:
+	"https://charmingprogrammer.github.io/All-Manoarya-Images/Navigation.png",
+	PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-make-Sidebar-with-multiple-Pages-using-HTML-CSS-and-Javascript.html",
+	alt: "Navigation by Manoarya",
+	trending: "true",
+},
+	{
+		category: "HTM",
+		Title: "How to make Modal Box using HTML,CSS and Javascript?",
+		Paragraph:
+		"Learn How to make a awesome Modal Box with free source code.",
+		ImageUrl:
+		"https://cdn.pixabay.com/photo/2021/12/08/17/04/modalbox-6856321_1280.jpg",
+		PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_modal_box_using_html_css_and_js.html",
+		alt: "Modal Box by Manoarya",
+		trending: "true",
+	},
+	{
+		category: "HTM",
+		Title: "How to make Information opener using HTML, CSS and Js? for free.",
+		Paragraph:
+		"Learn How to make a awesome Information opener with free source code.",
+		ImageUrl:
+		"https://i.ibb.co/Hn2wVnv/Pics-Art-12-09-08-14-16.png",
+		PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_info_opener_using_html_css_and_js.html",
+		alt: "Information opener by Manoarya",
+		trending: "true",
+	},
+
+
+];
