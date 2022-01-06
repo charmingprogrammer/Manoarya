8fa7de4f4fc15991becc68ebae446c5e980214c4
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-06 11:34:12 +0530

    update Class_11th_Physics_Easy_Notes_by_manoarya.js

--- Class_11th_Physics_Easy_Notes_by_manoarya.js
+++ Class_11th_Physics_Easy_Notes_by_manoarya.js
@@ -3,8 +3,30 @@ const AllNotes = [
   Title: "1.Physical World",
   ImgUrl: "https://i.ibb.co/CQjVBJY/20220104-123904-0000.png",
   ImgAlt: "Physical world",
-  Topic: `<h2></h2>
-  <p>
-  </p>`
+  Topic: `
+  <h2>Know About Physics</h2>
+  
+  <h2>Scientific methods<h2> 
+  
+  <h2>Natural Science<h2>
+  <h2>Unification<h2>
+  <h2>Reduction<h2>
+  <h2>Reductionism<h2>
+  <h2>Scope and craze of physics<h2>
+  <h2>Classical Physics<h2>
+  <h2>Mechanics<h2>
+  <h2>Electrodynamics<h2>
+  <h2>Optics<h2>
+  <h2>Thermodynamics<h2>
+  <h2>Physics tech and society<h2>
+  <h2>Fundamentals forces in nature<h2>
+  <h2>Gravitational Forces<h2>
+  <h2>Electromagnetic Forces<h2>
+  <h2>Strong Nuclear Forces<h2>
+  <h2>Weak Nuclear Forces<h2>
+  <h2>Towards Unification Of Forces<h2>
+  <h2>Nature Of Physical Laws<h2>
+  
+  `
 },
 ]
