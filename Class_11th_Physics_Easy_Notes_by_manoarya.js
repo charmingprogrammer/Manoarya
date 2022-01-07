06588865e1f75da6dd20cc647d5a8a044088858c
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-07 11:04:40 +0530

    update Class_11th_Physics_Easy_Notes_by_manoarya.js

--- Class_11th_Physics_Easy_Notes_by_manoarya.js
+++ Class_11th_Physics_Easy_Notes_by_manoarya.js
@@ -1,31 +1,30 @@
-const AllNotes = [
-  {
+const AllNotes = [{
   Title: "1.Physical World",
   ImgUrl: "https://i.ibb.co/CQjVBJY/20220104-123904-0000.png",
   ImgAlt: "Physical world",
-  Topic: ` 
+  Topic: `
   <div class="infos">
   <h2>Know About Science</h2>
-  <p>Science is a careful and defth observation of natural phenomenon. <br><br> Word science come from Latin verb 'Scientica'. <br> Scientica meaning id 'To know'. </p> 
+  <p>Science is a careful and defth observation of natural phenomenon. <br><br> Word science come from Latin verb 'Scientica'. <br> Scientica meaning id 'To know'. </p>
   </div>
   <div class="infos">
-  <h2>Scientific methods</h2> 
+  <h2>Scientific methods</h2>
   <p>Method or rules of scientific research is called scientific method. <br><br> <b>Some scientific method are following. </b><br><br>1. Systematic observations <br>2. Controlled experiments. <br> 3. Qualitative and quantitative reasoning <br>4. Mathematics modeling <br>5. Predictions <br>6. Verificationof theory <br><br> To apply one science theory or formula. First check that this theory of formula is complete all the steps of scientific method and also philosophical debate by many scientists and philosophers. </p>
-  </div> 
-  <div class="infos"> 
+  </div>
+  <div class="infos">
   <h2>Natural Science</h2>
   <p>What we study in physics chemistry and biology all the stuff known as natural science. <br><br> <b>Physics :</b> Physics  is the study of nature and its law. <br><br> Word physics comes from Greek Word which meaning is natural. <br> <br><b>Two principles of physics.</b><br> <br>1. Unification <br>2. Reduction</p>
-  </div> 
-  <div class="infos"> 
+  </div>
+  <div class="infos">
   <h2>Unification</h2>
-  <p>Try to explain and understand natural phenomena in same theory or formula. </p> 
-  </div> 
-  <div class="infos"> 
+  <p>Try to explain and understand natural phenomena in same theory or formula. </p>
+  </div>
+  <div class="infos">
   <h2>Reduction</h2>
   <p>Try to explain complex natural phenomena to  simpler way known as reduction.</p>
-</div>
-<div class="infos"> 
-<h2>Scope and craze of physics</h2>
-</div>`
+  </div>
+  <div class="infos">
+  <h2>Scope and craze of physics</h2>
+  </div>`
 },
 ]
