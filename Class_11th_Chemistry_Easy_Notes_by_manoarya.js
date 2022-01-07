32f620ca28ce7cdf954964b70b012efe8b5a77b3
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-07 11:06:10 +0530

    update Class_11th_Chemistry_Easy_Notes_by_manoarya.js

--- Class_11th_Chemistry_Easy_Notes_by_manoarya.js
+++ Class_11th_Chemistry_Easy_Notes_by_manoarya.js
@@ -1,10 +1,8 @@
 const AllNotes = [
   {
-  Title: "1.Basic Concept Of Chemistry",
-  ImgUrl: "https://i.ibb.co/CQjVBJY/20220104-123904-0000.png",
-  ImgAlt: "Physical world",
-  Topic: `<h2></h2>
-  <p>
-  </p>`
+  Title: "1. Basic Concept Of Chemistry",
+  ImgUrl: "https://i.ibb.co/b5Pmsp9/png-20220107-110241-0000.png",
+  ImgAlt: "Basic concept of Chemistry",
+  Topic: `<div class="infos"><h2>Heading 01</h2><p>Paragraphs</p></div> `
 },
 ]
