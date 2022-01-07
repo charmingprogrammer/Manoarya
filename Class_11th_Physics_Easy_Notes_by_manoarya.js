c1f3f17b575e5f055f1ae2b84f0c694db8d66b53
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-07 09:57:02 +0530

    update Class_11th_Physics_Easy_Notes_by_manoarya.js

--- Class_11th_Physics_Easy_Notes_by_manoarya.js
+++ Class_11th_Physics_Easy_Notes_by_manoarya.js
@@ -3,33 +3,29 @@ const AllNotes = [
   Title: "1.Physical World",
   ImgUrl: "https://i.ibb.co/CQjVBJY/20220104-123904-0000.png",
   ImgAlt: "Physical world",
-  Topic: `
+  Topic: ` 
+  <div class="infos">
   <h2>Know About Science</h2>
   <p>Science is a careful and defth observation of natural phenomenon. <br><br> Word science come from Latin verb 'Scientica'. <br> Scientica meaning id 'To know'. </p> 
+  </div>
+  <div class="infos">
   <h2>Scientific methods</h2> 
   <p>Method or rules of scientific research is called scientific method. <br><br> <b>some scientific method are following. </b><br>1. Systematic observations <br>2. Controlled experiments. <br> 3. Qualitative and quantitative reasoning <br>4. Mathematics modeling <br>5. Predictions <br>6. Verificationof theory <br><br> To apply one science theory or formula. First check that this theory of formula is complete all the steps of scientific method and also philosophical debate by many scientists and philosophers. </p>
+  </div> 
+  <div class="infos"> 
   <h2>Natural Science</h2>
   <p>What we study in physics chemistry and biology all the stuff known as natural science. <br><br> <b>Physics</b>: Physics  is the study of nature and its law. <br><br> Word physics comes from Greek Word which meaning is natural. <b>Two principles of physics.</b> <br>1. Unification <br>2. Reduction</p>
+  </div> 
+  <div class="infos"> 
   <h2>Unification</h2>
   <p>Try to explain and understand natural phenomena in same theory or formula. </p> 
+  </div> 
+  <div class="infos"> 
   <h2>Reduction</h2>
   <p>Try to explain complex natural phenomena to  simpler way known as reduction.</p>
-  
-  <h2>Scope and craze of physics</h2>
-  <h2>Classical Physics</h2>
-  <h2>Mechanics</h2>
-  <h2>Electrodynamics</h2>
-  <h2>Optics</h2>
-  <h2>Thermodynamics</h2>
-  <h2>Physics tech and society</h2>
-  <h2>Fundamentals forces in nature</h2>
-  <h2>Gravitational Forces</h2>
-  <h2>Electromagnetic Forces</h2>
-  <h2>Strong Nuclear Forces</h2>
-  <h2>Weak Nuclear Forces</h2>
-  <h2>Towards Unification Of Forces</h2>
-  <h2>Nature Of Physical Laws</h2>
-  
-  `
+</div>
+<div class="infos"> 
+<h2>Scope and craze of physics</h2>
+</div>`
 },
 ]
