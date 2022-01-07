41bf1f6bad2f252309c8c9633e7ac5e99a504eca
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-07 11:46:06 +0530

    update English_Grammer.js

--- English_Grammer.js
+++ English_Grammer.js
@@ -1,10 +1,8 @@
 const AllNotes = [
   {
   Title: "1. Letter",
-  ImgUrl: "https://i.ibb.co/CQjVBJY/20220104-123904-0000.png",
-  ImgAlt: "Physical world",
-  Topic: `<h2></h2>
-  <p>
-  </p>`
+  ImgUrl: "https://i.ibb.co/bLm10zk/20220107-114332-0000.png",
+  ImgAlt: "Letter",
+  Topic: `<div class="infos"><h2>Heading 01</h2><p>Paragraphs</p></div>`
 },
 ]
