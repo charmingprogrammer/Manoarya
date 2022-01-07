044220fef026ab3a2bf5369999e9eff2540ff88c
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-07 08:37:37 +0530

    update Class_11th_Physics_Easy_Notes_by_manoarya.js

--- Class_11th_Physics_Easy_Notes_by_manoarya.js
+++ Class_11th_Physics_Easy_Notes_by_manoarya.js
@@ -4,14 +4,17 @@ const AllNotes = [
   ImgUrl: "https://i.ibb.co/CQjVBJY/20220104-123904-0000.png",
   ImgAlt: "Physical world",
   Topic: `
-  <h2>Know About Physics</h2>
-  
+  <h2>Know About Science</h2>
+  <p>Science is a careful and defth observation of natural phenomenon. <br><br> Word science come from Latin verb 'Scientica'. <br> Scientica meaning id 'To know'. </p> 
   <h2>Scientific methods<h2> 
-  
+  <p>Method or rules of scientific research is called scientific method. <br><br> <b>some scientific method are following. </b><br>1. Systematic observations <br>2. Controlled experiments. <br> 3. Qualitative and quantitative reasoning <br>4. Mathematics modeling <br>5. Predictions <br>6. Verificationof theory <br><br> To apply one science theory or formula. First check that this theory of formula is complete all the steps of scientific method and also philosophical debate by many scientists and philosophers. </p>
   <h2>Natural Science<h2>
+  <p><b>Two principles of physics.</b> <br>1. Unification <br>2. Reduction</p>
   <h2>Unification<h2>
+  <p>Try to explain and understand natural phenomena in same theory or formula. </p> 
   <h2>Reduction<h2>
-  <h2>Reductionism<h2>
+  <p>Try to explain complex natural phenomena to  simpler way known as reduction.</p>
+  
   <h2>Scope and craze of physics<h2>
   <h2>Classical Physics<h2>
   <h2>Mechanics<h2>
