ad676957d7bd7beb59a725d2e28b0d1b905748e1
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-07 12:53:46 +0530

    update Computer_Fundamentals.js

--- Computer_Fundamentals.js
+++ Computer_Fundamentals.js
@@ -1,7 +1,7 @@
 const AllNotes = [
   {
   Title: "1. Computer Introduction",
-  ImgUrl: "https://i.ibb.co/YPWB7FX/20220107-121951-0000.png",
+  ImgUrl: "https://i.ibb.co/7rHRxW3/20220107-125220-0000.png",
   ImgAlt: "Computer Introduction",
   Topic: `<div class="infos"><h2>Heading 01</h2><p>Paragraphs</p></div>`
 },
