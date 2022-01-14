ffbe012e29f93b66b686ae10fbc94cc7997a66b6
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-14 12:21:29 +0530

    update Class_11th_maths_Easy_Notes_by_manoarya.js

--- Class_11th_maths_Easy_Notes_by_manoarya.js
+++ Class_11th_maths_Easy_Notes_by_manoarya.js
@@ -3,6 +3,6 @@ const AllNotes = [
   Title: "1. Sets",
   ImgUrl: "https://i.ibb.co/jbMJbhS/20220107-112239-0000.png",
   ImgAlt: "Sets",
-  Topic: `<div class="infos"><h2>Heading 01</h2><p>Paragraphs</p></div>`
+  Topic: `<div class="infos"><h2>What is sets ?</h2><p>Set is a well defined collection of objects. <br><br><b>Other meaning of sets : </b>Collection, Aggregate, class etc. <br> <br> Sets denotes by capital letters like A, B, C, D etc. <br><br>Elements denotes by small letters like a, b, c, d etc. <br><br><b>Example : </b>The collection of vowel in english alphabet. <br> <b>Answer : </b>This sets contains five elements a, e, i, o, u. </p></div>`
 },
 ]
