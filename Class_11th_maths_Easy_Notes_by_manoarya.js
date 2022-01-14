804d1719ac1b0263f5f7a38be4f435afe3eaacbd
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-14 12:58:35 +0530

    update Class_11th_maths_Easy_Notes_by_manoarya.js

--- Class_11th_maths_Easy_Notes_by_manoarya.js
+++ Class_11th_maths_Easy_Notes_by_manoarya.js
@@ -1,10 +1,16 @@
-const AllNotes = [
-  {
+const AllNotes = [{
   Title: "1. Sets",
   ImgUrl: "https://i.ibb.co/jbMJbhS/20220107-112239-0000.png",
   ImgAlt: "Sets",
   Topic: `<div class="infos"><h2>What is sets ?</h2><p>Set is a well defined collection of objects. <br><br><b>Other meaning of sets : </b>Collection, Aggregate, class etc. <br> <br> Sets denotes by capital letters like A, B, C, D etc. <br><br>Elements denotes by small letters like a, b, c, d etc. <br><br><b>Example : </b>The collection of vowel in english alphabet. <br> <b>Answer : </b>This sets contains five elements a, e, i, o, u. </p></div><div class="infos"><h2>Reserve letters for sets</h2><p>
-  
+  <b>N : </b>For the set of natural numbers.  <br>
+  <b>Z : </b> For the set of integers. <br>
+  <b>Z+ : </b> For the set of positive integers. <br>
+  <b>Q : </b>For the set of all rational numbers. <br>
+  <b>Q+ : </b>For the set of all positive rational numbers. <br>
+  <b>R : </b>For the set of all real numbers. <br>
+  <b>R+ : </b>For the set of all positive real numbers. <br>
+  <b>C : </b>For the set of all complex numbers.
   </p></div>  <br> `
 },
 ]
