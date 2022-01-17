cd00f06269fb743bb72e86d7f44a701cd95f9a06
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-17 22:22:47 +0530

    update Class_11th_maths_Easy_Notes_by_manoarya.js

--- Class_11th_maths_Easy_Notes_by_manoarya.js
+++ Class_11th_maths_Easy_Notes_by_manoarya.js
@@ -11,6 +11,102 @@ const AllNotes = [{
   <b>R : </b>For the set of all real numbers. <br>
   <b>R+ : </b>For the set of all positive real numbers. <br>
   <b>C : </b>For the set of all complex numbers.
-  </p></div> <div class="infos"><h2>Descriptions of sets</h2><p>Set described in 2 terms. <br><br>1. Roster Form or tabular from <br>2. Set-builder form <br><br></p></div><div class="infos"><h2>1. Roster Form or tabular Form</h2><p>In this form a set is describe by listing elements, separated  with commas(,) within bracket{}. <br><br> <b>Example : </b>The set of vowels in english alphabet. <br><b>In Roster from  : </b>{a, e, i, o, u}</p></div> <div class="infos"><h2>Set-Builder form</h2><p>In  this form a set is described by characterizing property p(x) of  its elements x. <br> <br> <b>Simple structure  of Set-builder  form </b> <br> <br> {x:P(x)holds} <br> {x|P(x)holds} <br> <br> <b> '|' or ':'</b> symbol read as <b>'Such that'</b> <br> <br> <b>Example : </b>The set E of all even natural numbers. <br> <b> In Set-builder form : </b>E = {x:x is a natural numbers and x = 2n for n∈N}</p></div><br>  `
+  </p></div> <div class="infos"><h2>Descriptions of sets</h2><p>Set described in 2 terms. <br><br>1. Roster Form or tabular from <br>2. Set-builder form <br><br></p></div><div class="infos"><h2>1. Roster Form or tabular Form</h2><p>In this form a set is describe by listing elements, separated  with commas(,) within bracket{}. <br><br> <b>Example : </b>The set of vowels in english alphabet. <br><b>In Roster from  : </b>{a, e, i, o, u}</p></div> <div class="infos"><h2>Set-Builder form</h2><p>In  this form a set is described by characterizing property p(x) of  its elements x. <br> <br> <b>Simple structure  of Set-builder  form </b> <br> <br> {x:P(x)holds} <br> {x|P(x)holds} <br> <br> <b> '|' or ':'</b> symbol read as <b>'Such that'</b> <br> <br> <b>Example : </b>The set E of all even natural numbers. <br> <b> In Set-builder form : </b>E = {x:x is a natural numbers and x = 2n for n∈N}</p></div>
+  <div class="infos"><h2>Types of sets</h2><p>1. Empty Set
+2. Singleton Set
+3. Finite Set
+4. Infinite Set
+5. Equivalent set
+6. Equal Set</p></div> 
+<div class="infos"><h2>1. Empty Set :</h2><p>A set is said to be empty or null or void set if it has no elements. <br> <br>Empty Sets denoted by (symbole). <br><br>Empty sets denoted in roster form like this {}.
+Example : {x}</p></div> 
+
+
+<div class="infos"><h2>2. Singleton Sets :</h2><p>A set consisting of a single element is called a singleton set. <br> <br> 
+<b> Example : </b>The set {5} is a singleton set.</p></div> 
+
+<div class="infos"><h2>3. Finite Sets :</h2><p>A set called finite set if it is either void set or ites element can be listed (counted) by natural numbers 1,2,3...and the process of listing terminates at a certain natural number (n). <br> <br> 
+<b>
+Each one of the following is a finite set : </b> <br> <br> 
+1. Set of even natural numbers less than 100. <br> 
+2.Set of soldiers in the Indian army. <br> 
+3. Set of all our people on the earth. <br><br> 
+<b>What is a cardinal number ?</b><br> <br> A number denoting quantity (one, two, three, etc.), as opposed to an ordinal number (first, second, third, etc.).
+<br> <br> 
+
+<b>Cardinal numbers of finite sets : </b> The number ‘n’ in the above definition is called the cardinal number or order of a finite ‘A’ and its denoted by <b>n(A)</b>.
+</p></div> 
+
+<div class="infos"><h2>4. Infinite Set : </h2><p>A set whose element cannot be listed by natural numbers 1,2,3….n, for any natural number ‘n’ is called infinite sets. <br> <br> 
+<b>Each one of the following sets is infinite set : </b> <br> <br> 
+1. Set of all points in a plane. <br> 
+2. Set of all lines in a plane. <br>
+3. {x ∈ R : 0 < x < 1}
+</p></div> 
+<div class="infos"><h2>5. Equivalent Sets : </h2><p>Two finite sets ‘A’ and ‘B’ and equivalent if their cardinal numbers are the same. <br> <br> 
+<b>Example : </b>n(A) = n(B)
+</p></div> 
+
+<div class="infos"><h2>6. Equal Set : </h2><p>Two sets ‘A’ and ‘B’ are said to be equal if every element of  ‘A’ is  a member of ‘B’, and every element of ‘B’ is a member of ‘A’. <br> <br> 
+When ‘A’ and ‘B’ are equal then we write like this A = B . <br>
+And if not equal then we write like this A =/= B. <br> <br> 
+<b>Example :</b><br> A = {5, 2, 1, 6}<br>B = {6, 5, 2, 1} <br> <br> 
+Set ‘A’ and ‘B’ are equal sets right ! <br> 
+So, A = B
+</p></div> 
+
+<div class="infos"><h2>Different Between equivalent set and equal set.</h2><p>A = {5,2,1,6}<br> 
+B = {6,5,2,1} <br> <br> 
+Set ‘A’ and ‘B’ have the same number of elements and same elements so both sets are equivalent and equal sets. <br> <br> 
+
+<b>Look at the following example : </b><br><br> 
+C = {1,2,3,4} <br> 
+D = {a,b,c,d} <br> <br> 
+
+Sets ‘C’ and ‘D’ have the same number of elements, that's why sets ‘C’ and ‘D’ are equivalent sets. <br> <br> 
+
+But Sets ‘C’ and ‘D’ are not equal sets because both do not have the same elements. <br> <br> 
+<b>
+Equal sets are equivalent set but equivalent sets are not equal sets.</b>
+</p></div>
+
+<div class="infos"><h2>Subset : </h2><p>Let A and B be two sets if every element of A is an element of B then A is called a subset of B. <br> <br> 
+
+If A is a subset of B then we write like this : A ⊆ B and read as “ A is a subset of B ” or “A is contained in B”. <br> <br> 
+
+<b>Super set :</b> If A is a subset if B, we say that B contains A or B is a superset of A and we write B⊃A. <br> <br> 
+
+If A is not a subset of B, we write A⊄B.
+<br> <br> 
+Every set is a subset of itself.
+<br> <br> 
+Empty sets are subsets of every set. <br> <br> 
+For Example : {1} = {1,2,3,4,5}
+</p></div>
+
+ <div class="infos"><h2>Universal Set : </h2><p>A set that contains all sets in a given context is called that universal set.
+<br> <br> 
+<b>Example : </b>When we are using sets containing natural numbers, then N is the Universal Set. <br> <br> 
+
+A = {1,2,3}<br> 
+B = {2,4,5,6}<br> 
+C = {1,3,5,7}<br> <br> 
+
+Universal Set (U) = {1,2,3,4,5,6,7}<br> <br> 
+Universal set denoted by <b>‘U’.</b>
+</p></div> 
+
+
+<div class="infos"><h2>Power Set : </h2><p>Let A be a set. then the collection of family of all subsets of A is called the power set of A and  is denoted by p(A). <br> <br> 
+
+that is , P(A) = { S : S ⊂ A }
+<br> <br> 
+<b>Power set of a given set is always non-empty.</b>
+</p></div> 
+ 
+ <div class="infos"><h2>Venn Diagrams : </h2><p>In venn diagrams the universal set(U) is represented by points within a rectangle and its subsets are represented by points in closed curves (usually circle) within the rectangle. <br> 
+ <img src = "" alt = "Venn diagram by manoarya">
+</p></div>
+  <br>`
 },
 ]
