a39cf229d89870b70bd8b40d0cefeaed6397a2bf
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-01-18 21:39:26 +0530

    update Class_11th_Physics_Easy_Notes_by_manoarya.js

--- Class_11th_Physics_Easy_Notes_by_manoarya.js
+++ Class_11th_Physics_Easy_Notes_by_manoarya.js
@@ -25,6 +25,47 @@ const AllNotes = [{
   </div>
   <div class="infos">
   <h2>Scope and craze of physics</h2>
-  </div>`
+  </div>
+  <div class="infos"><h2>Two domains we have to get some idea of the scope of physics.</h2><p>
+1. Macroscopic <br> 
+2. Microscopic <br> <br> 
+
+<b>1. Macroscopic : </b>The macroscopic domain includes phenomena at laboratory and astronomical scales. <br> <br> 
+
+<b>2. Microscopic :</b>The microscopic domain includes atomic, molicular and nuclear phenomena.
+</p></div> 
+
+<div class="infos"><h2>Classical Physics :</h2><p>Classical physics deals with macroscopic phenomena and include subject like mechinics, electrodynamic, optics and thermodynamic.</p></div> 
+
+<div class="infos"><h2>Fundamental forces in nature.</h2><p>
+1. Gravitational Forces <br> 
+2. Electromagnetic Forces <br> 
+3. Strong Nuckear Forces <br> 
+4. Weak Nuclear Forces <br>
+</p></div> 
+
+<div class="infos"><h2>1. Gravitational Forces :</h2><p>The gravitational force is the force of mutual attraction between two objects by property of their masses. <br> <br> 
+
+
+Gravitational Force is the universal force. <br> 
+Means, every object on the universe experience gravitational force.
+</p></div> 
+
+<div class="infos"><h2>2. Electromagnetic Force :</h2><p>The Electromagnetic Forces is the force between charged particles. <br><br>
+
+Electromagnetic forces are more powerful as compair to gravitational force. <br> <br> 
+
+Gravity is always attract, but Elcotomegnatic force can be attract or repulse.
+</p></div> 
+ 
+<div class="infos"><h2>3. Strong Nuclear Force :</h2><p>The strong nuclear force binds protons and neutrons in a nucleus. <br> <br> 
+Strong Nuclear Force is the strongest force as compair to all fundamental forces.
+</p></div> 
+
+<div class="infos"><h2>4. Weak Nuclear Force :</h2><p>The weak nuclear force appears only in certain nuclear process such as the B-dacay of a nucleus. <br> <br> 
+
+
+The Weak Nuclear Force is the weak nuclear force but not weak from gravitational force.</p></div> 
+  <br> `
 },
 ]
