fa46244729f9389a6e48cdd582e3b459bf862a4b
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-07-02 16:29:15 +0530

    Replace URLs

--- AddPost.js
+++ AddPost.js
@@ -6,7 +6,7 @@ const AllPost = [
       "Explanation about basic html that you use almost every html project.",
       ImageUrl:
       "https://i.ibb.co/JyTRSns/20220205-101420-0000.png",
-      PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/Html-basic-explanation-by-manoarya.html",
+      PostUrl: "https://manoarya.com/Manoarya%20All%20Post/Html-basic-explanation-by-manoarya.html",
       alt: "html basic explanation by Manoarya",
       trending: "true",
   },
@@ -18,7 +18,7 @@ const AllPost = [
       "Learn How to make a awesome sidebar with multiple Pages for free source code.",
       ImageUrl:
       "https://charmingprogrammer.github.io/All-Manoarya-Images/Navigation.png",
-      PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-make-Sidebar-with-multiple-Pages-using-HTML-CSS-and-Javascript.html",
+      PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-make-Sidebar-with-multiple-Pages-using-HTML-CSS-and-Javascript.html",
       alt: "Navigation by Manoarya",
       trending: "true",
   },
@@ -30,7 +30,7 @@ const AllPost = [
           "Learn How to make a awesome Modal Box with free source code.",
           ImageUrl:
           "https://cdn.pixabay.com/photo/2021/12/08/17/04/modalbox-6856321_1280.jpg",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_modal_box_using_html_css_and_js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How_to_make_modal_box_using_html_css_and_js.html",
           alt: "Modal Box by Manoarya",
           trending: "true",
       },
@@ -41,7 +41,7 @@ const AllPost = [
           "Learn How to make a awesome Information opener with free source code.",
           ImageUrl:
           "https://i.ibb.co/Hn2wVnv/Pics-Art-12-09-08-14-16.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_info_opener_using_html_css_and_js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How_to_make_info_opener_using_html_css_and_js.html",
           alt: "Information opener by Manoarya",
           trending: "true",
       },
@@ -52,7 +52,7 @@ const AllPost = [
           "Learn How to make Sign Up and Login from using HTML, CSS and Js? with free source code.",
           ImageUrl:
           "https://i.ibb.co/86Xtq9w/Pics-Art-12-11-07-22-32.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_sign_up_from_using_html_css_and_js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How_to_make_sign_up_from_using_html_css_and_js.html",
           alt: "SignUp and Login from Manoarya",
           trending: "true",
       },
@@ -63,7 +63,7 @@ const AllPost = [
           "Learn How to make image preview using html css and javascript? with free source code.",
           ImageUrl:
           "https://i.ibb.co/vV17WF2/Pics-Art-12-12-07-21-29.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-make-image-preview-using-html-css-and-javascript.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-make-image-preview-using-html-css-and-javascript.html",
           alt: "Image preview by Manoarya",
           trending: "true",
       },
@@ -74,7 +74,7 @@ const AllPost = [
           "Learn How to add text on image using html and css? with free source code.",
           ImageUrl:
           "https://i.ibb.co/qnP3BrW/Pics-Art-12-12-07-39-39.jpg",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-add-text-on-image-using-html-css-and-js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-add-text-on-image-using-html-css-and-js.html",
           alt: "Add text on image by Manoarya",
           trending: "true",
       },
@@ -85,7 +85,7 @@ const AllPost = [
           "Learn How to make a image gallery using html css and js? with free source code.",
           ImageUrl:
           "https://i.ibb.co/f4qj2Nh/Pics-Art-12-12-08-48-15.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-make-a-image-gallery-using-html-css-and-js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-make-a-image-gallery-using-html-css-and-js.html",
           alt: "Image gallery by Manoarya",
           trending: "true",
       },
@@ -96,7 +96,7 @@ const AllPost = [
           "Learn How to create a responsive layout using html css and js? with free source code.",
           ImageUrl:
           "https://i.ibb.co/C1JSCnS/Pics-Art-12-12-08-51-45.png ",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-create-a-responsive-layout-using-html-css-and-js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-responsive-layout-using-html-css-and-js.html",
           alt: "Responsive layout by Manoarya",
           trending: "true",
       },
@@ -107,7 +107,7 @@ const AllPost = [
           "Learn How to create a search ui like a browser window using html and css?  with free source code.",
           ImageUrl:
           "https://i.ibb.co/KhZnT9j/Pics-Art-12-12-08-55-12.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-create-a-search-ui-like-a-browser-window-using-html-css-and-js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-search-ui-like-a-browser-window-using-html-css-and-js.html",
           alt: "browser window ui by Manoarya",
           trending: "true",
       },
@@ -118,7 +118,7 @@ const AllPost = [
           "Learn How to create a horizontal slider using html and css? with free source code.",
           ImageUrl:
           "https://i.ibb.co/qkV4gBL/Pics-Art-12-12-08-59-01.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-create-a-horizontal-slider-using-html-css-and-js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-horizontal-slider-using-html-css-and-js.html",
           alt: "Horizontal slider by Manoarya",
           trending: "true",
       },
@@ -129,7 +129,7 @@ const AllPost = [
           "Learn How to create a profile ui using html and css? with free source code.",
           ImageUrl:
           "https://i.ibb.co/qRCDMgW/Pics-Art-12-12-09-00-24.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-create-a-profile-ui-using-html-css-and-js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-profile-ui-using-html-css-and-js.html",
           alt: "Prifile UI by Manoarya",
           trending: "true",
       },
@@ -140,7 +140,7 @@ const AllPost = [
           "Learn How to make full page navigation using 3 steps? with free source code.",
           ImageUrl:
           "https://i.ibb.co/HVPs5Th/Pics-Art-12-14-07-14-52.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-make-full-page-navigation-using-3-steps.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-make-full-page-navigation-using-3-steps.html",
           alt: "full page navigation by Manoarya",
           trending: "true",
       },
@@ -151,7 +151,7 @@ const AllPost = [
           "Learn Sportify UI Clone  Using HTML and CSS. with free source code.",
           ImageUrl:
           "https://i.ibb.co/b7K2M4t/Pics-Art-12-14-10-32-12.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/Sportify_UI_clone_by_manoarya.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/Sportify_UI_clone_by_manoarya.html",
           alt: "Sportify Ui Clone Manoarya",
           trending: "true",
       },
@@ -162,7 +162,7 @@ const AllPost = [
           "Learn Google Play Store UI Clone using HTML and CSS. with free source code.",
           ImageUrl:
           "https://i.ibb.co/Q63YMyL/Pics-Art-12-15-11-16-35.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/Play_store_UI_clone_by_manoarya.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/Play_store_UI_clone_by_manoarya.html",
           alt: "Google Play Clone Manoarya",
           trending: "true",
       },
@@ -173,7 +173,7 @@ const AllPost = [
           "Learn Color Tic tac to using html css and js. with free source code.",
           ImageUrl:
           "https://i.ibb.co/TqvVRrB/Pics-Art-12-16-08-05-18.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/Color_tic_tac_to_using_html_css_and_js.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/Color_tic_tac_to_using_html_css_and_js.html",
           alt: "color tic tac to",
           trending: "true",
       },
@@ -184,7 +184,7 @@ const AllPost = [
           "Learn How to make Paytm UI Clone using html and css? with free source code.",
           ImageUrl:
           "https://i.ibb.co/Vx3mcXT/Pics-Art-12-17-06-57-19.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How_to_make_paytm_ui_clone_using_html_and_css.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How_to_make_paytm_ui_clone_using_html_and_css.html",
           alt: "Paytm UI Clone ",
           trending: "true",
       },
@@ -195,7 +195,7 @@ const AllPost = [
           "Learn How to make snap scrolling effects using html and css. with free source code.",
           ImageUrl:
           "https://i.ibb.co/R9J5K6R/Pics-Art-12-18-07-05-31.png",
-          PostUrl: "https://charmingprogrammer.github.io/Manoarya/Manoarya%20All%20Post/How-to-make-snap-scrolling-effects-using-html-and-css.html",
+          PostUrl: "https://manoarya.com/Manoarya%20All%20Post/How-to-make-snap-scrolling-effects-using-html-and-css.html",
           alt: "snap scrolling Effect",
           trending: "true",
       },
