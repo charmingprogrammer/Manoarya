c3450c6ff60d6949ab76377ccbad89e99ac9c1be
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-07-31 09:20:03 -0700

    some change

--- Computer_Cources/PostData.js
+++ Computer_Cources/PostData.js
@@ -4,10 +4,5 @@ const PostData = [
     Date: "01 April 2022",
     PostUrl: "Posts/day1.html",
   },
-  {
-    Day: "Day 2",
-    Date: "02 April 2022",
-    PostUrl: "Posts/day1.html",
-  },
   
 ];
