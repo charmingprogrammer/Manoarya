223222e1204259834930266260aebadeec35e7af
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-07-31 09:01:14 -0700

    Post Created

---
+++ Computer_Cources/PostData.js
@@ -0,0 +1,13 @@
+const PostData = [
+  {
+    Day: "Day 1",
+    Date: "01 April 2022",
+    PostUrl: "Posts/day1.html",
+  },
+  {
+    Day: "Day 2",
+    Date: "02 April 2022",
+    PostUrl: "Posts/day1.html",
+  },
+  
+];
