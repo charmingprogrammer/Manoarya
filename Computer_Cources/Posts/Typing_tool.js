f29ba4d194ab2cba9bcb409b5dfc5b5401e956af
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-08-01 09:05:40 -0700

    typing tool updated

--- Computer_Cources/Posts/Typing_tool.js
+++ Computer_Cources/Posts/Typing_tool.js
@@ -1,6 +1,32 @@
 // initializing the variables
-const Input_ffff =  document.getElementById('ffff');
-const Input_FFFF =  document.getElementById('FFFF');
+const Input_ffff = document.getElementById("ffff");
+const Input_FFFF = document.getElementById("FFFF");
+const typing_playground = document.getElementById("typing_playground");
 
+// fetch the keyData from the keyData.js
 
-// set a function for checking the correct typing
+for (let i = 0; i < keyData.length; i++) {
+  const element = keyData[i];
+  typing_playground.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="playgrounds">
+    <h2>${element.title}</h2>
+    <input type="search" placeholder="Type" id="${element.key}">
+</div>
+    `
+  );
+}
+
+const inputs = typing_playground.querySelectorAll("input");
+
+for (let a = 0; a < inputs.length; a++) {
+  const AllInputs = inputs[a];
+  AllInputs.addEventListener("keypress", (event) => {
+    if (event.key === AllInputs.id) {
+      document.getElementById(AllInputs.id).style.backgroundColor = "white";
+    } else {
+      document.getElementById(AllInputs.id).style.backgroundColor = "#e41313c2";
+    }
+  });
+}
---
+++ Computer_Cources/Posts/keyData.js
@@ -0,0 +1,35 @@
+const keyData = [
+    {
+        key: 'f',
+        title : 'ffff',
+    },
+    {
+        key: 'F',
+        title : 'FFFF',
+    },
+    {
+        key: 'd',
+        title : 'dddd',
+    },
+    {
+        key: 'D',
+        title : 'DDDD',
+    },
+    {
+        key: 's',
+        title : 'ssss',
+    },
+    {
+        key: 'S',
+        title : 'SSSS',
+    },
+    {
+        key: 'a',
+        title : 'aaaa',
+    },
+    {
+        key: 'A',
+        title : 'AAAA',
+    },
+
+]
