42add15034e256dc3dceab09cdf9033eaa3ad5c4
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-07-31 19:20:25 -0700

    typing tools added

---
+++ Computer_Cources/Posts/Typing_tool.js
@@ -0,0 +1,6 @@
+// initializing the variables
+const Input_ffff =  document.getElementById('ffff');
+const Input_FFFF =  document.getElementById('FFFF');
+
+
+// set a function for checking the correct typing
