b0665fc1465216365a879d0181341fa4d35258bd
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-08-02 06:02:08 -0700

    day 2

--- Computer_Cources/PostData.js
+++ Computer_Cources/PostData.js
@@ -1,8 +1,13 @@
 const PostData = [
   {
     Day: "Day 1",
-    Date: "01 August 2022",
+    Date: "02 August 2022",
     PostUrl: "Posts/day1.html",
+  },
+  {
+    Day: "Day 2",
+    Date: "03 August 2022",
+    PostUrl: "Posts/day2.html",
   },
   
 ];
