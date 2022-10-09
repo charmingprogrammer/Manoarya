491a5aae209ec654d6bf855b8b44bdc002fd4b17
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-10-09 12:39:33 +0530

    Create Slider_img.js

---
+++ Apna_coaching/Slider_img.js
@@ -0,0 +1,19 @@
+  // Array for img list
+  const ImgSrc = [
+    {
+      src: "https://cdn.pixabay.com/photo/2022/10/03/12/03/microphone-7495739__340.jpg",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2022/10/03/12/03/microphone-7495739__340.jpg",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2022/10/03/12/03/microphone-7495739__340.jpg",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2022/10/03/12/03/microphone-7495739__340.jpg",
+    },
+    {
+      src: "https://cdn.pixabay.com/photo/2022/10/03/12/03/microphone-7495739__340.jpg",
+    },
+  ];
+  export default ImgSrc;
