32969b1f65fbc7948042ab3309cdd49e05a13fac
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-12-13 08:28:41 +0530

    img src changed

--- AddPost.js
+++ AddPost.js
@@ -2,7 +2,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "HTML basic explanation by Manoarya",
-    ImageUrl: "https://i.ibb.co/JyTRSns/20220205-101420-0000.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/Html-basic-explanation-by-manoarya.html",
     alt: "html basic explanation by Manoarya",
@@ -12,8 +12,7 @@ const AllPost = [
     category: "HTM",
     Title:
       "How to make Sidebar with multiple Pages using HTML, CSS and Javascript?",
-    ImageUrl:
-      "https://i.ibb.co/wgXMnPx/full-page-nav-bar.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-make-Sidebar-with-multiple-Pages-using-HTML-CSS-and-Javascript.html",
     alt: "Navigation by Manoarya",
@@ -23,8 +22,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make Modal Box using HTML,CSS and Javascript?",
-    ImageUrl:
-      "https://cdn.pixabay.com/photo/2021/12/08/17/04/modalbox-6856321_1280.jpg",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How_to_make_modal_box_using_html_css_and_js.html",
     alt: "Modal Box by Manoarya",
@@ -32,7 +30,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make Information opener using HTML, CSS and Js? for free.",
-    ImageUrl: "https://i.ibb.co/Hn2wVnv/Pics-Art-12-09-08-14-16.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How_to_make_info_opener_using_html_css_and_js.html",
     alt: "Information opener by Manoarya",
@@ -41,8 +39,8 @@ const AllPost = [
     category: "HTM",
     Title:
       "How to make Sign Up and Login from using HTML, CSS and Js? for free.",
-    ImageUrl: "https://i.ibb.co/86Xtq9w/Pics-Art-12-11-07-22-32.png",
-    PostUrl:
+    ImageUrl:"logo.png",
+      PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How_to_make_sign_up_from_using_html_css_and_js.html",
     alt: "SignUp and Login from Manoarya",
     recommend:'yes',
@@ -50,7 +48,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make image preview using html css and javascript?",
-    ImageUrl: "https://i.ibb.co/vV17WF2/Pics-Art-12-12-07-21-29.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-make-image-preview-using-html-css-and-javascript.html",
     alt: "Image preview by Manoarya",
@@ -59,7 +57,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to add text on image using html and css?",
-    ImageUrl: "https://i.ibb.co/qnP3BrW/Pics-Art-12-12-07-39-39.jpg",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-add-text-on-image-using-html-css-and-js.html",
     alt: "Add text on image by Manoarya",
@@ -67,7 +65,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make a image gallery using html css and js?",
-    ImageUrl: "https://i.ibb.co/f4qj2Nh/Pics-Art-12-12-08-48-15.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-make-a-image-gallery-using-html-css-and-js.html",
     alt: "Image gallery by Manoarya",
@@ -76,7 +74,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to create a responsive layout using html css and js?",
-    ImageUrl: "https://i.ibb.co/C1JSCnS/Pics-Art-12-12-08-51-45.png ",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-responsive-layout-using-html-css-and-js.html",
     alt: "Responsive layout by Manoarya",
@@ -85,8 +83,8 @@ const AllPost = [
     category: "HTM",
     Title:
       "How to create a search ui like a browser window using html and css? ",
-    ImageUrl: "https://i.ibb.co/KhZnT9j/Pics-Art-12-12-08-55-12.png",
-    PostUrl:
+    ImageUrl:"logo.png",
+      PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-search-ui-like-a-browser-window-using-html-css-and-js.html",
     alt: "browser window ui by Manoarya",
     recommend:'yes',
@@ -94,7 +92,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to create a horizontal slider using html and css?",
-    ImageUrl: "https://i.ibb.co/qkV4gBL/Pics-Art-12-12-08-59-01.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-horizontal-slider-using-html-css-and-js.html",
     alt: "Horizontal slider by Manoarya",
@@ -102,7 +100,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to create a profile ui using html and css?",
-    ImageUrl: "https://i.ibb.co/qRCDMgW/Pics-Art-12-12-09-00-24.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-create-a-profile-ui-using-html-css-and-js.html",
     alt: "Prifile UI by Manoarya",
@@ -110,7 +108,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make full page navigation using 3 steps?",
-    ImageUrl: "https://i.ibb.co/HVPs5Th/Pics-Art-12-14-07-14-52.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-make-full-page-navigation-using-3-steps.html",
     alt: "full page navigation by Manoarya",
@@ -118,7 +116,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "Sportify UI Clone  Using HTML and CSS.",
-    ImageUrl: "https://i.ibb.co/b7K2M4t/Pics-Art-12-14-10-32-12.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/Sportify_UI_clone_by_manoarya.html",
     alt: "Sportify Ui Clone Manoarya",
@@ -126,7 +124,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "Google Play Store UI Clone using HTML and CSS.",
-    ImageUrl: "https://i.ibb.co/Q63YMyL/Pics-Art-12-15-11-16-35.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/Play_store_UI_clone_by_manoarya.html",
     alt: "Google Play Clone Manoarya",
@@ -135,7 +133,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "Color Tic tac to using html css and js.",
-    ImageUrl: "https://i.ibb.co/TqvVRrB/Pics-Art-12-16-08-05-18.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/Color_tic_tac_to_using_html_css_and_js.html",
     alt: "color tic tac to",
@@ -143,7 +141,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make Paytm UI Clone using html and css?",
-    ImageUrl: "https://i.ibb.co/Vx3mcXT/Pics-Art-12-17-06-57-19.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How_to_make_paytm_ui_clone_using_html_and_css.html",
     alt: "Paytm UI Clone ",
@@ -152,7 +150,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make snap scrolling effects using html and css.",
-    ImageUrl: "https://i.ibb.co/R9J5K6R/Pics-Art-12-18-07-05-31.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-make-snap-scrolling-effects-using-html-and-css.html",
     alt: "snap scrolling Effect",
@@ -160,7 +158,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make search bar and suggestion using only html.html",
-    ImageUrl: "https://i.ibb.co/wzFT4NC/Pics-Art-12-18-07-37-53.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How-to-make-search-bar-and-suggestion-using-only-html.html",
     alt: "search suggestions",
@@ -169,7 +167,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to create details opener and closer using only html.",
-    ImageUrl: "https://i.ibb.co/JKcN9FM/Pics-Art-12-18-07-42-37.png",
+    ImageUrl:"logo.png",
     PostUrl:"https://manoarya.com/Manoarya%20All%20Post/How-to-create-details-opener-and-closer-using-only-html.html",
     alt: "details opener closed",
   },
@@ -177,7 +175,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "Theme Design 01 by Manoarya",
-    ImageUrl: "https://i.ibb.co/27X0XQH/Picsart-22-02-02-13-33-34-247.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/Web_theme_1_by_manoarya.html",
     alt: "Web_theme_1_by_manoarya",
@@ -185,7 +183,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "Theme Design 02 by Manoarya",
-    ImageUrl: "https://i.ibb.co/YpfTLSL/Picsart-22-02-02-12-53-12-218.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/Web_theme_2_by_manoarya.html",
     alt: "Web_theme_2_by_manoarya",
@@ -193,7 +191,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "Theme Design 03 by Manoarya",
-    ImageUrl: "https://i.ibb.co/3pCvFrq/png-20220203-125048-0000.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/Web_theme_3_by_manoarya.html",
     alt: "Web_theme_3_by_manoarya",
@@ -201,7 +199,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "Code A Gallery web application using Pixabay API.",
-    ImageUrl: "https://i.ibb.co/8Ywd4JD/png-20220215-113300-0000.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/Create_a_web_gallery_using_pixabay_api_by_Manoarya.html",
     alt: "Create_a_web_gallery_using_pixabay_api_by_Manoarya.html",
@@ -210,7 +208,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "Javascript Array Demo Using A Projects.|By Manoarya",
-    ImageUrl: "https://i.ibb.co/sj3SJNn/20220218-212721-0000.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/Javascript-Array-Demo-Using-A-Projects.html",
     alt: "Javascript-Array-Demo-Using-A-Projects",
@@ -219,7 +217,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "How To Get Random Data From An Array Using Js.| by Manoarya",
-    ImageUrl: "https://i.ibb.co/ZmjcWFc/20220224-192953-0000.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/how_to_get_random_data_from_an_array_using_js.html",
     alt: "How To Get Random Data From An Array Using Js.| by Manoarya",
@@ -227,7 +225,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "CODING PROJECT IDEA: This Is What Professionals Do",
-    ImageUrl: "https://i.ibb.co/f4HDCLN/Pics-Art-12-17-07-10-57.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/CODING-PROJECT-IDEA-This-Is-What-Professionals-Do.html",
     alt: "CODING-PROJECT-IDEA-This-Is-What-Professionals-Do.html",
@@ -235,7 +233,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "Tips For New Developers By Manoarya.",
-    ImageUrl: "https://i.ibb.co/m5s3JQX/20220203-220051-0000.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/Tips-for-new-developers-by-manoarya.html#",
     alt: "Tips For New Developers By Manoarya.",
@@ -243,7 +241,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "What is an API? Simply Explain with project.",
-    ImageUrl: "https://i.ibb.co/dgJq0Rm/20220207-081830-0000.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya All Post/What-is-API-simply-explain-with-project.html",
     alt: "What is an API? Simply Explain with project.",
@@ -251,14 +249,14 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "What is the Web development? Simply Explain",
-    ImageUrl: "https://i.ibb.co/rxjN0vG/png-20220209-120259-0000.png",
+    ImageUrl:"logo.png",
     PostUrl: "https://manoarya.com/Manoarya All Post/Who-is-Web-developer.html",
     alt: "Who-is-Web-developer.html",
   },
   {
     category: "CheatSheet",
     Title: "HTML Complete Cheat Sheet By Manoaya.",
-    ImageUrl: "https://i.ibb.co/BLNjByQ/html.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/HTML%20Complete%20Cheat%20Sheet%20By%20Manoaya.html",
     alt: "html cheatsheet",
@@ -266,7 +264,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Side bar with multiple pages by manoarya.",
-    ImageUrl: "https://i.ibb.co/wgXMnPx/full-page-nav-bar.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Navigation.html",
     alt: "Nav bar with multiple pages by manoarya.",
@@ -274,7 +272,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Modal Box",
-    ImageUrl: "https://cdn.pixabay.com/photo/2021/12/08/17/04/modalbox-6856321_1280.jpg",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/ModalBox.html",
     alt: "Nav bar with multiple pages by manoarya.",
@@ -282,7 +280,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Sign Up and Login",
-    ImageUrl: "https://i.ibb.co/86Xtq9w/Pics-Art-12-11-07-22-32.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Sign_Up_From.html",
     alt: "Sign Up and Login Demo",
@@ -290,7 +288,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Information opener",
-    ImageUrl: "https://i.ibb.co/Hn2wVnv/Pics-Art-12-09-08-14-16.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/InfoOpen.html",
     alt: "Information opener",
@@ -298,7 +296,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Image preview ",
-    ImageUrl: "https://i.ibb.co/vV17WF2/Pics-Art-12-12-07-21-29.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Img_preview.html",
     alt: "Image preview ",
@@ -306,7 +304,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Add text on image",
-    ImageUrl: "https://i.ibb.co/qnP3BrW/Pics-Art-12-12-07-39-39.jpg",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Text_on_img.html",
     alt: "add text on image",
@@ -314,7 +312,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Image gallery",
-    ImageUrl: "https://i.ibb.co/f4qj2Nh/Pics-Art-12-12-08-48-15.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Img_gallery.html",
     alt: "image gallery",
@@ -322,7 +320,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo |Responsive layout",
-    ImageUrl: "https://i.ibb.co/C1JSCnS/Pics-Art-12-12-08-51-45.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/layout.html",
     alt: "responsive layout",
@@ -330,7 +328,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Create a search ui like a browser window",
-    ImageUrl: "https://i.ibb.co/KhZnT9j/Pics-Art-12-12-08-55-12.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Browser_window.html",
     alt: "create a search ui like a browser window",
@@ -338,7 +336,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Horizontal slider ",
-    ImageUrl: "https://i.ibb.co/qkV4gBL/Pics-Art-12-12-08-59-01.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Img_preview.html",
     alt: "horizontal slider",
@@ -347,7 +345,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Create a profile ui",
-    ImageUrl: "https://i.ibb.co/qRCDMgW/Pics-Art-12-12-09-00-24.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Profile.html",
     alt: "create a profile ui",
@@ -355,7 +353,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Full page navigation",
-    ImageUrl: "https://i.ibb.co/HVPs5Th/Pics-Art-12-14-07-14-52.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Page_navigation.html",
     alt: "full page navigation",
@@ -363,7 +361,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Sportify UI Clone",
-    ImageUrl: "https://i.ibb.co/b7K2M4t/Pics-Art-12-14-10-32-12.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Sportify_ui_clone.html",
     alt: "Sportify UI Clone",
@@ -371,7 +369,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Google Play Store UI Clone",
-    ImageUrl: "https://i.ibb.co/Q63YMyL/Pics-Art-12-15-11-16-35.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Play_store_ui_clone.html",
     alt: "Google Play Store UI Clone",
@@ -379,7 +377,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Color Tic tac to",
-    ImageUrl: "https://i.ibb.co/TqvVRrB/Pics-Art-12-16-08-05-18.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Color_tic_tac_to.html",
     alt: "Color Tic tac to",
@@ -387,7 +385,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Paytm UI Clone",
-    ImageUrl: "https://i.ibb.co/Vx3mcXT/Pics-Art-12-17-06-57-19.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Paytm_ui_clone.html",
     alt: "Paytm UI Clone",
@@ -395,7 +393,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Snap scrolling effects",
-    ImageUrl: "https://i.ibb.co/R9J5K6R/Pics-Art-12-18-07-05-31.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Snap_Scrolling.html",
     alt: "snap scrolling effects",
@@ -403,7 +401,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Make search bar and suggestion",
-    ImageUrl: "https://i.ibb.co/wzFT4NC/Pics-Art-12-18-07-37-53.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Search_Input.html",
     alt: "make search bar and suggestion",
@@ -411,7 +409,7 @@ const AllPost = [
   {
     category: "Demo",
     Title: "Demo | Create details opener and closer using only html.",
-    ImageUrl: "https://i.ibb.co/JKcN9FM/Pics-Art-12-18-07-42-37.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Demo/Details_tag.html",
     alt: "create details opener and closer using only html.",
@@ -419,7 +417,7 @@ const AllPost = [
   {
     category: "Explanation",
     Title: "Deploy the react web app on the github using 4 steps by manoarya.",
-    ImageUrl: "https://manoarya.com/Images/Deploy%20React%20website.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/Deploy_React_Web_App_On_The_Github_Using_3_Steps.html",
     alt: "Deploy the react web app on the github using 4 steps by manoarya.",
@@ -427,7 +425,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "How to make Html search bar with suggestions using html,css and javascript.",
-    ImageUrl: "https://manoarya.com/Images/html.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/How_to_make_Html_search_bar_with_suggestions_using_html_css_and_javascript.html",
     alt: "How to make Html search bar with suggestions using html,css and javascript.",
@@ -435,7 +433,7 @@ const AllPost = [
   {
     category: "HTM",
     Title: "Amazon UI clone created by Manoarya using html css and js.",
-    ImageUrl: "../../Images/Amazon UI clone poster.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "https://manoarya.com/Manoarya%20All%20Post/Amazon_UI_Clone_using_HTML_CSS_JS.html",
     alt: "Amazon UI clone created by Manoarya using html css and js.",
@@ -443,10 +441,9 @@ const AllPost = [
   {
     category: "Demo",
     Title: "[Demo] Amazon UI clone created by Manoarya using html css and js.",
-    ImageUrl: "../../Images/Amazon UI clone poster.png",
+    ImageUrl:"logo.png",
     PostUrl:
       "./Demo/Amazon UI Clone/index.html",
     alt: "Amazon UI clone created by Manoarya using html css and js.",
   },
 ];
-
